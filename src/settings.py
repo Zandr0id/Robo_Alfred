afe8731fb0cbd1146c7f5efbce889c6a5622ef4c
Zane Youmans <zyoumans@TheBatComputer.localdomain>
2019-11-02 22:41:22 -0700

    Removed personal info from settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,4 @@
 HOST = 'irc.twitch.tv'
-NICK = 'havok_bot' #This doesn't do anything
-PASS = 'oauth:jjjmizzezoudurczt9k554oiyhlvez'
-CHANNEL = 'zandr0id_'
+NICK = 'random' #This doesn't do anything
+PASS =   #your oauth key 
+CHANNEL = #your channel
